9cca72d491391c2cf27b1d6e0bb6046a5397c962
programadorsamuca <samuelrochapimentel06@gmail.com>
2025-03-13 14:19:11 +0000

    escrita do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 int main() {
+    //Declaração de variaveis
+   
+    char Estado;
+    char Carta[50];
+    char Cidade[50];
+    int Populacao;
+    float Area;
+    float PIB;
+    int Turismo;
     
+    //Entrada e saída de dados das cartas
+   
+    printf("Digite o Estado: \n");
+    scanf("%c", &Estado);
+
+    printf("Digite o Código da Carta: \n");
+    sacnf("%s", &Carta);
+
+    printf("Digite o Nome da Cidade: \n");
+    sacnf("%s", &Cidade);
+
+    printf("Digite a população total: \n");
+    sacnf("%d", &Populacao);
+
+    printf("Digite a Área por km: \n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    prinf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &Turismo);
+
+    //impressão dos resultados no terminal
+
+    printf("")
+
+
     
     return 0;
 }
